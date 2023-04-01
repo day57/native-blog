51782385aaf7cd4b5a56e338ba2f3c288c5a5081
day57 <theday057@gmail.com>
2023-03-31 21:42:57 -0700

    changes

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children}) =>  {
+    
+    const [blogPosts , setBlogPosts ] = useState([]);
+
+    const addBlogPost = () => {
+        // create a new array , then take all the blogPosts and put them into the new array
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}  ])
+    }
+
     return ( 
-        <BlogContext.Provider
-            value={5}
-        >
+        <BlogContext.Provider value={{data:blogPosts, addBlogPost}} >
             {children}
         </BlogContext.Provider>
     )
